ffdff816a7f15d43725bbcb1f06743f649a29c35
Aman Sharma <aman.sharma@ilink-systems.com>
2025-09-02 19:49:09 +0530

    a

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ import os
 # Use DATABASE_URL from Render, or fallback to your Postgres URL
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "ppostgresql://raksha_360_iyyc_user:cnmxNVfakZunRQGmY94OIog0rDxyGpZp@dpg-d2rfn1euk2gs7389acgg-a.singapore-postgres.render.com/raksha_360_iyyc"
+    "postgresql://raksha_360_iyyc_user:cnmxNVfakZunRQGmY94OIog0rDxyGpZp@dpg-d2rfn1euk2gs7389acgg-a/raksha_360_iyyc"
 )
 
 # Create engine
