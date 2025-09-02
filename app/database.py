f4dc0426abb935de7d7023fc29d86c442f1b2990
Aman Sharma <aman.sharma@ilink-systems.com>
2025-09-02 15:50:38 +0530

    a

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://raksha_360_user:IXSbIC6uSyPwpUgHc2toiijhMwYFuQle@dpg-d2rc8mv5r7bs73bru9ig-a/raksha_360")
 
 engine = create_engine(DATABASE_URL, echo=True, future=True)
 meta = MetaData()
